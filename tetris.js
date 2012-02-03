cbf6940ddf3e22f006f5a52a0119a1cf71c3720f
Johnicholas Hines <johnicholas.hines@gmail.com>
2012-02-02 19:28:36 -0800

    a way to win and a background

--- tetris.js
+++ tetris.js
@@ -14,11 +14,7 @@ var tetris = function (jaws) {
 	    for (var row = 0; row < this.height; ++row) {
 		this.map.push([]);
 		for (var col = 0; col < this.width; ++col) {
-		    if (random(1)) {
-			this.map[row].push('red');
-		    } else {
-			this.map[row].push('green');
-		    }
+		    this.map[row].push('black');
 		}
 	    }
 	    this.row = 0;
@@ -26,7 +22,10 @@ var tetris = function (jaws) {
 	    jaws.preventDefaultKeys(['left', 'right', 'up', 'down']);
 	    this.interval = 800; // milliseconds?
 	    var that = this;
+	    this.ticking = true;
 	    setTimeout(function () { that.tick(); }, this.interval);
+	    this.background = new Image();
+	    this.background.src = 'background.png';
 	},
 	tick: function () {
 	    var old_row = this.row;
@@ -37,13 +36,31 @@ var tetris = function (jaws) {
 		// fix up
 		this.row -= 1;
 		this.map[this.row][this.col] = 'blue';
+		for (var i = this.height - 1; i >= 0; --i) {
+		    var found_complete_row = true;
+		    for (var j= 0; j < this.width; j++) {
+			if (this.map[i][j] == 'black') {
+			    found_complete_row = false;
+			}
+		    }
+		    if (found_complete_row) {
+			alert('you win!');
+			this.ticking = false;
+		    }
+		}
 		this.row = 0;
 		this.col = Math.floor(this.width / 2);
+		if (this.collision()) {
+		    alert('game over: loss');
+		    this.ticking = false;
+		}
 	    } else {
 		// nothing to do?
 	    }
-	    var that = this;
-	    setTimeout(function () { that.tick(); }, this.interval);
+	    if (this.ticking) {
+		var that = this;
+		setTimeout(function () { that.tick(); }, this.interval);
+	    }
 	},
 	collision: function () {
 	    if (this.row >= this.height ||
@@ -72,8 +89,7 @@ var tetris = function (jaws) {
 	},
 	draw: function () {
 	    jaws.context.save();
-	    jaws.context.fillStyle = 'black';
-	    jaws.context.fillRect(0, 0, jaws.width, jaws.height);
+	    jaws.context.drawImage(this.background, 0, 0);
 	    jaws.context.translate((jaws.width - this.width * this.cellsize)/ 2,
 				   (jaws.height - this.height * this.cellsize)/ 2);
 	    jaws.context.scale(this.cellsize, this.cellsize);
@@ -83,23 +99,25 @@ var tetris = function (jaws) {
 	    jaws.context.restore();
 	},
 	update: function () {
-	    var old_row = this.row;
-	    var old_col = this.col;
-	    if (jaws.pressed('left')) {
-		this.col -= 1;
-	    }
-	    if (jaws.pressed('right')) {
-		this.col += 1;
-	    }
-	    if (jaws.pressed('down')) {
-		this.row += 1;
-	    }
-	    if (jaws.pressed('up')) {
-		// you can't go up in tetris!
-	    }
-	    if (this.collision()) {
-		this.row = old_row;
-		this.col = old_col;
+	    if (this.ticking) {
+		var old_row = this.row;
+		var old_col = this.col;
+		if (jaws.pressed('left')) {
+		    this.col -= 1;
+		}
+		if (jaws.pressed('right')) {
+		    this.col += 1;
+		}
+		if (jaws.pressed('down')) {
+		    this.row += 1;
+		}
+		if (jaws.pressed('up')) {
+		    // you can't go up in tetris!
+		}
+		if (this.collision()) {
+		    this.row = old_row;
+		    this.col = old_col;
+		}
 	    }
 	}
     }
